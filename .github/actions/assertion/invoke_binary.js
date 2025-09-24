d56321223bd5ea534cf54385fbc0d5be44cb60b3
Jakub Jarosz <j.jarosz@f5.com>
2025-09-24 11:45:48 +0100

    wip - setup log level from env var
    Signed-off-by: Jakub Jarosz <j.jarosz@f5.com>

--- .github/actions/assertion/invoke_binary.js
+++ .github/actions/assertion/invoke_binary.js
@@ -2,7 +2,7 @@ const childProcess = require('child_process')
 const os = require('os')
 const process = require('process')
 
-const VERSION = 'eedf8cf'
+const VERSION = 'ba2b96c'
 
 function chooseBinary() {
     const platform = os.platform()
